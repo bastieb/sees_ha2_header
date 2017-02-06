e1e1ea92e23ada623a9e439948e75318abc8053f
bastieb <bastie.b@web.de>
2017-02-06 14:57:03 +0100

    some elementary format corrections in Frontcar.h

--- FrontCar.h
+++ FrontCar.h
@@ -39,25 +39,37 @@ SC_MODULE(FrontCar){
 	void car2()
 	{
 		//Variablen
-		i=0;sensor_range=251.0;action_range=100.0;
+		i = 0;
+		sensor_range = 251.0;
+		action_range = 100.0;
 			
-		if(car_sighted==true || (dist<300 && tempomatstatus==1)){
+		if(car_sighted == true || (dist < 300 && tempomatstatus == 1)){
 
-			v_acc=v_car;dist=dist+(v_car-v_current);
+			v_acc = v_car;
+			dist = dist + v_car - v_current;
 
 			//Damit dist nur beim ersten betreten auf 250 gesetzt wird		
-			if (dist>300){dist=250;i=1;}
+			if (dist > 300){
+				dist = 250;
+				i = 1;
+			}
 			//Außerhalb der Reichweite, ACC wir nicht aktiviert
-			if (dist>action_range && dist <= sensor_range)in_range=false;				
+			if (dist > action_range && dist <= sensor_range){
+				in_range = false;
+			}				
 			//In der Reichweite, ACC wird aktiviert
-			if (dist<=action_range && dist>0){
-			in_range=true;	
+			if (dist <= action_range && dist > 0){
+				in_range = true;	
 			}
 			next_trigger(1,SC_SEC);
 		}
 		//muss noch eine bessere Loesung her, Variable i dient nur dazu, 		
 		//dass er beim ersten mal betreten nicht wieder dist auf 301 setzt	
-		if(dist>sensor_range && i==0){ dist=301, in_range=false; i=0;}	
+		if(dist > sensor_range && i == 0){
+			dist = 301;
+			in_range = false; 
+			i = 0;
+		}	
 	}
 };
 
